be217632a2801415aa745b3081d6941cbceec22d
Jordan <43543240+HotGreenPeas@users.noreply.github.com>
2023-10-14 20:38:41 -0400

    sample python script with custom model

---
+++ phase1/YOLOv8InferenceClass.py
@@ -0,0 +1,82 @@
+import torch
+import numpy as np
+import cv2
+from time import time
+from ultralytics import YOLO
+import supervision as sv
+import os
+
+
+class ObjectDetection:
+
+    def __init__(self, capture_index):
+
+        self.capture_index = capture_index
+
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        print("Using Device: ", self.device)
+
+        self.model = self.load_model()
+
+        self.CLASS_NAMES_DICT = self.model.model.names
+
+        self.box_annotator = sv.BoxAnnotator(
+            sv.ColorPalette.default(), thickness=3, text_thickness=3, text_scale=1.5)
+
+    def load_model(self):
+
+        path = os.getcwd()
+        print(path)
+        model = YOLO("best.pt")  # load a pretrained YOLOv8n model
+        model.fuse()
+
+        return model
+
+    def predict(self, frame):
+
+        results = self.model(frame)
+
+        return results
+
+    def plot_bboxes(self, results, frame):
+        # Extract detections for person class
+        for result in results:
+            print(result.names)
+
+        return frame
+
+    def __call__(self):
+
+        cap = cv2.VideoCapture(self.capture_index)
+        assert cap.isOpened()
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+
+        while True:
+
+            start_time = time()
+
+            ret, frame = cap.read()
+            assert ret
+
+            results = self.predict(frame)
+            frame = self.plot_bboxes(results, frame)
+
+            end_time = time()
+            fps = 1/np.round(end_time - start_time, 2)
+
+            cv2.putText(frame, f'FPS: {int(fps)}', (20, 70),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0, 255, 0), 2)
+
+            cv2.imshow('YOLOv8 Detection', frame)
+
+            if cv2.waitKey(5) & 0xFF == 27:
+
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+
+detector = ObjectDetection(capture_index=0)
+detector()
